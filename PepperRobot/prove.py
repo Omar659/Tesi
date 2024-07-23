43549973de5111e394c22fadd5ca11f0e20d79a1
Omar659 <bayoumi.omar97@gmail.com>
2024-07-23 14:40:04 +0200

    prova

--- PepperRobot/prove.py
+++ PepperRobot/prove.py
@@ -33,35 +33,35 @@
 # print(device)
 
 
-# import torch
-# from PIL import Image
-# from transformers import AutoProcessor, AutoModelForCausalLM 
+import torch
+from PIL import Image
+from transformers import AutoProcessor, AutoModelForCausalLM 
 
-# device = "cuda:0" if torch.cuda.is_available() else "cpu"
-# torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
 
-# model = AutoModelForCausalLM.from_pretrained("./Florence-2-base", torch_dtype=torch_dtype, trust_remote_code=True).to(device)
-# processor = AutoProcessor.from_pretrained("./Florence-2-base", trust_remote_code=True)
+model = AutoModelForCausalLM.from_pretrained("./Florence-2-base", torch_dtype=torch_dtype, trust_remote_code=True).to(device)
+processor = AutoProcessor.from_pretrained("./Florence-2-base", trust_remote_code=True)
 
-# # url = "https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/transformers/tasks/car.jpg?download=true"
+# url = "https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/transformers/tasks/car.jpg?download=true"
 
-# def run_example(task_prompt, image, text_input=None):
-#     if text_input is None:
-#         prompt = task_prompt
-#     else:
-#         prompt = task_prompt + text_input
-#     inputs = processor(text=prompt, images=image, return_tensors="pt").to(device, torch_dtype)
-#     generated_ids = model.generate(
-#         input_ids=inputs["input_ids"],
-#         pixel_values=inputs["pixel_values"],
-#         max_new_tokens=1024,
-#         num_beams=3
-#     )
-#     generated_text = processor.batch_decode(generated_ids, skip_special_tokens=False)[0]
+def run_example(task_prompt, image, text_input=None):
+    if text_input is None:
+        prompt = task_prompt
+    else:
+        prompt = task_prompt + text_input
+    inputs = processor(text=prompt, images=image, return_tensors="pt").to(device, torch_dtype)
+    generated_ids = model.generate(
+        input_ids=inputs["input_ids"],
+        pixel_values=inputs["pixel_values"],
+        max_new_tokens=1024,
+        num_beams=3
+    )
+    generated_text = processor.batch_decode(generated_ids, skip_special_tokens=False)[0]
 
-#     parsed_answer = processor.post_process_generation(generated_text, task=task_prompt, image_size=(image.width, image.height))
+    parsed_answer = processor.post_process_generation(generated_text, task=task_prompt, image_size=(image.width, image.height))
 
-#     print(parsed_answer)    
+    print(parsed_answer)    
 
-# prompt = "<MORE_DETAILED_CAPTION>"
-# run_example(prompt)
+prompt = "<MORE_DETAILED_CAPTION>"
+run_example(prompt)
